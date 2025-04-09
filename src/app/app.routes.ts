5f2e240c1a95951bb4addd19d518afb946e3252c
MoniqChege <quichege33@gmail.com>
2025-04-09 15:20:12 +0300

    commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { SignupComponent } from './signup/signup.component';
+import { HomeComponent } from './home/home.component';
+import { SigninComponent } from './signin/signin.component';
+import { TaskCreationComponent } from './task-creation/task-creation.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent},
+    { path: 'signin', component: SigninComponent},
+    { path: 'signup', component: SignupComponent},
+    { path: 'task-creation', component: TaskCreationComponent},
+
+];
---
+++ src/app/task-creation/task-creation.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'task-creation',
+  imports: [RouterLink],
+  templateUrl: './task-creation.component.html',
+  styleUrl: './task-creation.component.css'
+})
+export class TaskCreationComponent {
+
+}
